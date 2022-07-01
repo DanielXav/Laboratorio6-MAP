b40d57a8d1f843534def6b400ebe68daf86ae476
Daniel Xavier <dxavier194@gmail.com>
2022-07-01 14:18:08 -0300

    second commit

--- src/Application.java
+++ src/Application.java
@@ -11,8 +11,17 @@ public class Application {
 		System.out.printf("%n------------------------------------------------%n%n");
 		
 		Triangulo isosceles = Triangulo.getInstancia();
+		isosceles.setLado1(10);
+		isosceles.setLado2(10);
+		isosceles.setLado3(5);
 		Triangulo equilatero = Triangulo.getInstancia();
+		equilatero.setLado1(10);
+		equilatero.setLado2(10);
+		equilatero.setLado3(10);
 		Triangulo retangulo = Triangulo.getInstancia();
+		retangulo.setLado1(13);
+		retangulo.setLado2(12);
+		retangulo.setLado3(5);
 		Triangulo triangulo = Triangulo.getInstancia();
 		
 		System.out.printf("%n------------------------------------------------%n%n");
--- src/Entities/Triangulo.java
+++ src/Entities/Triangulo.java
@@ -1,5 +1,8 @@
 package Entities;
 
+import Exceptions.FiguraComAtributoIgualAZeroException;
+import Exceptions.FiguraComAtributoNegativoException;
+
 public class Triangulo {
 
 	private static Triangulo instanciaUnica = null;
@@ -26,6 +29,12 @@ public class Triangulo {
 	}
 
 	public void setLado1(double lado1) {
+		if (lado1 < 0) {
+			throw new FiguraComAtributoNegativoException("Lado negativo não existe.");
+		}
+		else if (lado1 == 0) {
+			throw new FiguraComAtributoIgualAZeroException("Lado igual a 0 não existe.");
+		}
 		this.lado1 = lado1;
 	}
 
@@ -34,10 +43,22 @@ public class Triangulo {
 	}
 
 	public void setLado2(double lado2) {
+		if (lado2 < 0) {
+			throw new FiguraComAtributoNegativoException("Lado negativo não existe.");
+		}
+		else if (lado2 == 0) {
+			throw new FiguraComAtributoIgualAZeroException("Lado igual a 0 não existe.");
+		}
 		this.lado2 = lado2;
 	}
 
 	public double getLado3() {
+		if (lado3 < 0) {
+			throw new FiguraComAtributoNegativoException("Lado negativo não existe.");
+		}
+		else if (lado3 == 0) {
+			throw new FiguraComAtributoIgualAZeroException("Lado igual a 0 não existe.");
+		}
 		return lado3;
 	}
 
